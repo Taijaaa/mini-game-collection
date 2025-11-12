a14baf166e4d31f899639d6f8a0130b69463f2d3
Taija <000903701@mohawkcollege.ca>
2025-11-11 19:46:07 -0500

    add shoot add fix players

---
+++ unity-project/mini-game-collection/Assets/2025/Team11/Scripts/Balloon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGameCollection.Games2025.Team11
+{
+    public class Balloon : MiniGameBehaviour
+    {
+        public bool player1 = false;
+        
+    }
+}
--- unity-project/mini-game-collection/Assets/2025/Team11/Scripts/PlayerMovement.cs
+++ unity-project/mini-game-collection/Assets/2025/Team11/Scripts/PlayerMovement.cs
@@ -14,12 +14,19 @@ namespace MiniGameCollection.Games2025.Team11
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+
         }
 
         void Update()
         {
             moveInput = Vector2.zero;
 
+            //Target
+            if (ArcadeInput.Player1.Action1.Pressed)
+            {
+                Target();
+
+             }
             // Direct key checks
             if (ArcadeInput.Player1.Up.Held)
                 moveInput.y += 1;
@@ -38,5 +45,30 @@ namespace MiniGameCollection.Games2025.Team11
         {
             rb.velocity = moveInput * moveSpeed;
         }
+
+        void Target()
+
+        {
+            float radius = 0.1f;
+            Vector2 position = transform.position;
+            ContactFilter2D filter = new ContactFilter2D();
+            filter.useTriggers = true;
+            Collider2D[] colliders = new Collider2D[2];
+
+            int hits = Physics2D.OverlapCircle(position, radius, filter, colliders);
+
+            Debug.Log(hits);
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider == null) return;
+                if (collider.GetComponent<Balloon>() != null)
+                {
+                    if (collider.GetComponent<Balloon>().player1) Debug.Log("PLAYER 1");
+                    Destroy(collider.gameObject);
+                }
+            }
+        }
     }
+
+ 
 }
